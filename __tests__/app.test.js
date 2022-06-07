648ad930c8faee6c83af2a3a31467502d7aa3038
Andy-Mascaro <95321256+Andy-Mascaro@users.noreply.github.com>
2022-06-06 17:03:28 -0700

    maps were not matching from zodiac controller and test changed them to match

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const { zodiac } = require('../data/zodiac');
 
 describe('Backend-Zodiac-API routes', () => {
   beforeEach(() => {
@@ -9,6 +10,14 @@ describe('Backend-Zodiac-API routes', () => {
   });
 
   it('/zodiac should return a list of signs', async () => {
+    const res = await request(app).get('/zodiac');
+    const expected = zodiac.map((info) => {
+      return { id: info.id, name: info.name };
+    });
+    expect(res.body).toEqual(expected);
+  });
+
+  it('/zodiac/:id should return a sign detail', async () => {
     const res = await request(app).get('/zodiac/1');
     const aquarius = {
       id: '1',
